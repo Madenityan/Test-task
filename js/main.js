0d6f36d769b1ae8e81fe47e0c578364c0753dad3
unknown <marinakorsyn@gmail.com>
2018-07-02 12:46:19 +0300

    added the structure and progress bar

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+});
+
+
+$(".progress > span").each(function() {
+    $(this)
+        .data("origWidth", $(this).width())
+        .width(0)
+        .animate({
+            width: $(this).data("origWidth")
+        }, 1000);
+});
+
+
+
